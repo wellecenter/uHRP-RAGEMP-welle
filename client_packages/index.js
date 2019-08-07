b77aa2bc7e272d6d8f6a03f659dda3f397a16a5f
UNTAMEDHero <wekingsro@gmail.com>
2019-08-07 10:37:41 +0100

    Removing LightControl
    BUG Fixed Lightcontrol was in conflict with CEF so was not able to compile browser.
    Also removed Firedepartment for a while!

--- client_packages/index.js
+++ client_packages/index.js
@@ -1,5 +1,5 @@
-require('lightcontrol/index.js');
+//require('lightcontrol/index.js');
 require('nativeui/index.js');
 require('betternotifs/index.js');
-require('fire/fire/fire.js');
+//require('fire/fire/fire.js');
 require('gamemode/index.js');
